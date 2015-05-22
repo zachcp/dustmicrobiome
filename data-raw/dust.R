602210af1436b23b8217331eda8622ebcbd9810f
zach powers <zcharlop@rockefeller.edu>
2015-05-22 13:09:48 -0400

    update the fungi data to fix some invalid-unicode errors from R CMD Check

--- data-raw/dust.R
+++ data-raw/dust.R
@@ -52,6 +52,17 @@ sample_data(phy16S) <- as.data.frame(sampledata2)
 save(phy16S, file="data/phy16S.rda", compress = "xz")
 
 fungi = import_biom("data-raw/ITS_otu_table_wTax.biom")
+#from R CMD Check
+#* checking data for non-ASCII characters ... WARNING
+#Warning: found non-ASCII strings
+#'g__Epichlo<fd>' in object 'fungi'
+#'s__Epichlo<fd> glyceriae' in object 'fungi'
+# locations are levels(tax$Rank7)[2050] and levels(tax$Rank6)[493]
+tax <- data.frame(tax_table(fungi))
+levels(tax$Rank6)[493] <- "g__Epichlo"
+levels(tax$Rank7)[2050] <- "s__Epichlo glyceriae"
+tax_table(fungi) <- tax_table(as.matrix(tax))
+
 sample_data(fungi) <- as.data.frame(sampledata2)
 save(fungi, file="data/fungi.rda", compress = "xz")
 
